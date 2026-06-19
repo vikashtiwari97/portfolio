fef0f20b2b97faedd2ba4fa92e5f2925e137eefa
Vikash Tiwari <vikasrohtak2002@gmail.com>
2026-06-20 00:28:54 +0530

    footerfixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
     <footer className="text-white py-8 px-[12vw] md:px-[7vw] lg:px-[20vw]">
       <div className="container mx-auto text-center">
         {/* Name / Logo */}
-        <h2 className="text-xl font-semibold text-purple-500">Tarun Kaushik</h2>
+        <h2 className="text-xl font-semibold text-purple-500">Vikash Tiwari</h2>
 
         {/* Navigation Links - Responsive */}
         <nav className="flex flex-wrap justify-center space-x-4 sm:space-x-6 mt-4">
@@ -38,11 +38,10 @@ const Footer = () => {
         {/* Social Media Icons - Responsive */}
         <div className="flex flex-wrap justify-center space-x-4 mt-6">
           {[
-            { icon: <FaFacebook />, link: "https://www.facebook.com/tarun.kaushik.3511041/" },
-            { icon: <FaTwitter />, link: "https://twitter.com/CodingMaster6?s=09" },
-            { icon: <FaLinkedin />, link: "https://www.linkedin.com/in/tarun-kaushik-553b441a4" },
-            { icon: <FaInstagram />, link: "https://www.instagram.com/coding_.master/" },
-            { icon: <FaYoutube />, link: "https://www.youtube.com/codingmasteryt" },
+            { icon: <FaFacebook />, link: "https://www.facebook.com/profile.php?id=100010853449015" },
+            { icon: <FaLinkedin />, link: "https://www.linkedin.com/in/vikash-tiwari-7b5b2b244/" },
+            { icon: <FaInstagram />, link: "https://www.instagram.com/vikash_tiwari009/" },
+            { icon: <FaYoutube />, link: "https://www.youtube.com/@bcaslife9034" },
             
           ].map((item, index) => (
             <a
@@ -59,7 +58,7 @@ const Footer = () => {
 
         {/* Copyright Text */}
         <p className="text-sm text-gray-400 mt-6">
-          © 2025 Tarun Kaushik. All rights reserved.
+          © 2025 Vikash Tiwari. All rights reserved.
         </p>
       </div>
     </footer>
